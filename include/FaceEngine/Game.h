5e90b3390d4d6514f3e2d3caa1c49ae76ddedb27
silentptr <imquiet07@gmail.com>
2022-04-27 23:32:39 +1000

    added doxygen header

--- include/FaceEngine/Game.h
+++ include/FaceEngine/Game.h
@@ -1,3 +1,14 @@
+/**
+ * @file Game.h
+ * @author Logan Woods
+ * @brief A class containing the logic required to run a game.
+ * @version 0.1
+ * @date 2022-04-27
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
 #ifndef FACEENGINE_GAME_H_
 #define FACEENGINE_GAME_H_
 
