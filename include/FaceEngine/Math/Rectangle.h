5624d61d137cc3bee8439d8765aa3fd28c3bbf91
Sparky22296 <awesome22296@gmail.com>
2022-04-28 14:37:15 +1000

    Ported Rectangle over.

---
+++ include/FaceEngine/Math/Rectangle.h
@@ -0,0 +1,41 @@
+#ifndef FACEENGINE_MATH_RECTANGLE_H_
+#define FACEENGINE_MATH_RECTANGLE_H_
+
+#include "FaceEngine/Math/Vector2f.h"
+#include <iostream>
+
+namespace FaceEngine
+{
+    class Rectangle
+    {
+    public:
+        float X;
+        float Y;
+        float Width;
+        float Height;
+
+        Rectangle();
+        Rectangle(const Vector2f& position, const float& width, const float& height);
+        Rectangle(const float& x, const float& y, const float& width, const float& height);
+
+        float GetTop() const;
+        float GetBottom() const;
+        float GetLeft() const;
+        float GetRight() const;
+
+        Vector2f GetPosition() const;
+        Vector2f GetSize() const;
+
+        bool Contains(const Vector2f& vector) const;
+        bool CompletelyContains(const Vector2f& vector) const;
+        bool Contains(const Rectangle& rect) const;
+        bool CompletelyContains(const Rectangle& rect) const;
+        bool Intersects(const Rectangle& rect) const;
+        bool IntersectsOrTouches(const Rectangle& rect) const;
+        bool Equals(const Rectangle& rect) const;
+    };
+}
+
+std::ostream& operator <<(std::ostream& out, const FaceEngine::Rectangle& rect);
+
+#endif
---
+++ src/Math/Rectangle.cpp
@@ -0,0 +1,99 @@
+#include "FaceEngine/Math/Rectangle.h"
+#include <cmath>
+
+namespace FaceEngine
+{
+    Rectangle::Rectangle()
+    {
+        X = 0;
+        Y = 0;
+        Width = 0;
+        Height = 0;
+    }
+
+    Rectangle::Rectangle(const Vector2f& position, const float& width, const float& height)
+    {
+        X = position.X;
+        Y = position.Y;
+        Width = width;
+        Height = height;
+    }
+
+    Rectangle::Rectangle(const float& x, const float& y, const float& width, const float& height)
+    {
+        X = x;
+        Y = y;
+        Width = width;
+        Height = height;
+    }
+
+    float Rectangle::GetTop() const
+    {
+        return Y;
+    }
+
+    float Rectangle::GetBottom() const
+    {
+        return Y + Height;
+    }
+
+    float Rectangle::GetLeft() const
+    {
+        return X;
+    }
+
+    float Rectangle::GetRight() const
+    {
+        return X + Width;
+    }
+
+    Vector2f Rectangle::GetPosition() const
+    {
+        return Vector2f(X, Y);
+    }
+
+    Vector2f Rectangle::GetSize() const
+    {
+        return Vector2f(Width, Height);
+    }
+
+    bool Rectangle::Contains(const Vector2f& vector) const
+    {
+        return (vector.X >= X) && (vector.X <= GetRight()) && (vector.Y >= Y) && (vector.Y <= GetBottom());
+    }
+
+    bool Rectangle::CompletelyContains(const Vector2f& vector) const
+    {
+        return (vector.X > X) && (vector.X < GetRight()) && (vector.Y > Y) && (vector.Y < GetBottom());
+    }
+
+    bool Rectangle::Contains(const Rectangle& rect) const
+    {
+        return (X <= rect.X) && (GetRight() >= rect.GetRight()) && (Y <= rect.Y) && (GetBottom() >= rect.GetBottom());
+    }
+
+    bool Rectangle::CompletelyContains(const Rectangle& rect) const
+    {
+        return (X < rect.X) && (GetRight() > rect.GetRight()) && (Y < rect.Y) && (GetBottom() > rect.GetBottom());
+    }
+
+    bool Rectangle::Intersects(const Rectangle& rect) const
+    {
+        return (X < rect.GetRight()) && (GetRight() > rect.GetLeft()) && (Y < rect.GetBottom()) && (GetBottom() > rect.Y);
+    }
+
+    bool Rectangle::IntersectsOrTouches(const Rectangle& rect) const
+    {
+        return (X <= rect.GetRight()) && (GetRight() >= rect.GetLeft()) && (Y <= rect.GetBottom()) && (GetBottom() >= rect.Y);
+    }
+
+    bool Rectangle::Equals(const Rectangle& rect) const
+    {
+        return (X == rect.X) && (Y == rect.Y) && (Width == rect.Width) && (Height == rect.Height);
+    }
+}
+
+std::ostream& operator <<(std::ostream& out, const FaceEngine::Rectangle& rect)
+{
+    return out << "Rectangle:[X: " << rect.X << ", Y: " << rect.Y << ", Width: " << rect.Width << ", Height: " << rect.Height;
+}
