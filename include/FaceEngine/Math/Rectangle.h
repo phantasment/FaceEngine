f1dac3265b333eb6ae4999ffc50385a6bf462dee
silentptr <imquiet07@gmail.com>
2022-05-12 23:34:58 +1000

    fixed your shit retard

--- include/FaceEngine/Math/Rectangle.h
+++ include/FaceEngine/Math/Rectangle.h
@@ -3,6 +3,7 @@
 
 #include "FaceEngine/Math/Vector2.h"
 #include <iostream>
+#include <cstdint>
 
 namespace FaceEngine
 {
@@ -10,10 +11,10 @@ namespace FaceEngine
     class Rectangle
     {
         static_assert(std::is_same<float, T>() || 
-                      std::is_same<int, T>() || 
+                      std::is_same<std::int32_t, T>() || 
                       std::is_same<double, T>() || 
-                      std::is_same<short, T>() ||
-                      std::is_same<long, T>());
+                      std::is_same<std::int16_t, T>() ||
+                      std::is_same<std::int64_t, T>());
 
     public:
         T X;
@@ -21,27 +22,102 @@ namespace FaceEngine
         T Width;
         T Height;
 
-        Rectangle<T>();
-        Rectangle<T>(const Vector2f& position, const T width, const T height);
-        Rectangle<T>(const T x, const T y, const T width, const T height);
+        inline Rectangle()
+        {
+            X = 0;
+            Y = 0;
+            Width = 0;
+            Height = 0;
+        }
 
-        T GetTop() const;
-        T GetBottom() const;
-        T GetLeft() const;
-        T GetRight() const;
+        inline Rectangle(const Vector2f& position, const T width, const T height)
+        {
+            X = position.X;
+            Y = position.Y;
+            Width = width;
+            Height = height;
+        }
 
-        Vector2f GetPosition() const;
-        Vector2f GetSize() const;
+        inline Rectangle(const T x, const T y, const T width, const T height)
+        {
+            X = x;
+            Y = y;
+            Width = width;
+            Height = height;
+        }
 
-        bool Contains(const Vector2f& vector) const;
-        bool CompletelyContains(const Vector2f& vector) const;
-        bool Contains(const Rectangle<T>& rect) const;
-        bool CompletelyContains(const Rectangle<T>& rect) const;
-        bool Intersects(const Rectangle<T>& rect) const;
-        bool IntersectsOrTouches(const Rectangle<T>& rect) const;
-        bool Equals(const Rectangle<T>& rect) const;
+        inline T GetTop() const
+        {
+            return Y;
+        }
 
-        void operator +=(const Rectangle<T>& rect) noexcept;
+        inline T GetBottom() const
+        {
+            return Y + Height;
+        }
+
+        inline T GetLeft() const
+        {
+            return X;
+        }
+
+        inline T GetRight() const
+        {
+            return X + Width;
+        }
+
+        inline Vector2f GetPosition() const
+        {
+            return Vector2f(X, Y);
+        }
+
+        inline Vector2f GetSize() const
+        {
+            return Vector2f(Width, Height);
+        }
+
+        inline bool Contains(const Vector2f& vector) const
+        {
+            return (vector.X >= X) && (vector.X <= GetRight()) && (vector.Y >= Y) && (vector.Y <= GetBottom());
+        }
+
+        inline bool CompletelyContains(const Vector2f& vector) const
+        {
+            return (vector.X > X) && (vector.X < GetRight()) && (vector.Y > Y) && (vector.Y < GetBottom());
+        }
+
+        inline bool Contains(const Rectangle<T>& rect) const
+        {
+            return (X <= rect.X) && (GetRight() >= rect.GetRight()) && (Y <= rect.Y) && (GetBottom() >= rect.GetBottom());
+        }
+
+        inline bool CompletelyContains(const Rectangle<T>& rect) const
+        {
+            return (X < rect.X) && (GetRight() > rect.GetRight()) && (Y < rect.Y) && (GetBottom() > rect.GetBottom());
+        }
+
+        inline bool Intersects(const Rectangle<T>& rect) const
+        {
+            return (X < rect.GetRight()) && (GetRight() > rect.GetLeft()) && (Y < rect.GetBottom()) && (GetBottom() > rect.Y);
+        }
+
+        inline bool IntersectsOrTouches(const Rectangle<T>& rect) const
+        {
+            return (X <= rect.GetRight()) && (GetRight() >= rect.GetLeft()) && (Y <= rect.GetBottom()) && (GetBottom() >= rect.Y);
+        }
+
+        inline bool Equals(const Rectangle<T>& rect) const
+        {
+            return (X == rect.X) && (Y == rect.Y) && (Width == rect.Width) && (Height == rect.Height);
+        }
+
+        inline void operator+=(const Rectangle<T>& rect) noexcept
+        {
+            X = std::min(X, rect.X);
+            Y = std::min(Y, rect.Y);
+            Width = std::max(GetRight() - X, rect.GetRight() - X);
+            Height = std::max(GetBottom() - Y, rect.GetBottom() - Y);
+        }
     };
 }
 
