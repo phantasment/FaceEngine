12fdd53393801493163bd6b569380af093fe835d
silentptr <imquiet07@gmail.com>
2022-05-12 22:14:04 +1000

    texture2d

---
+++ include/FaceEngine/Texture2D.h
@@ -0,0 +1,52 @@
+#ifndef FACEENGINE_TEXTURE2D_H_
+#define FACEENGINE_TEXTURE2D_H_
+
+#include <cstdint>
+
+#include "FaceEngine/OGL.h"
+#include "FaceEngine/Resource.h"
+#include "FaceEngine/ResourceManager.h"
+#include "FaceEngine/Exception.h"
+
+namespace FaceEngine
+{
+    class Texture2D : public Resource
+    {
+    private:
+        GLuint handle;
+        std::uint32_t width, height;
+    
+        inline Texture2D(GLuint t, const std::uint32_t w, const std::uint32_t h) noexcept
+        {
+            handle = t;
+            width = w;
+            height = h;
+        }
+    public:
+        inline const bool IsDisposed() noexcept override
+        {
+            return handle == 0;
+        }
+
+        void Dispose() noexcept override;
+
+        inline const GLuint Handle() const noexcept
+        {
+            return handle;
+        }
+
+        inline const std::uint32_t Width() const noexcept
+        {
+            return width;
+        }
+
+        inline const std::uint32_t Height() const noexcept
+        {
+            return height;
+        }
+
+        static Texture2D* CreateTexture2D(ResourceManager*, const std::uint32_t, const std::uint32_t, std::uint8_t*);
+    };
+}
+
+#endif
---
+++ src/Texture2D.cpp
@@ -0,0 +1,34 @@
+#include "FaceEngine/Texture2D.h"
+
+namespace FaceEngine
+{
+    void Texture2D::Dispose() noexcept
+    {
+        if (handle != 0)
+        {
+            glDeleteTextures(1, &handle);
+            handle = 0;
+        }
+    }
+
+    Texture2D* Texture2D::CreateTexture2D(ResourceManager* rm, const std::uint32_t w, const std::uint32_t h, std::uint8_t* data)
+    {
+        if (w < 1 || h < 1)
+        {
+            throw Exception::FromMessage("FaceEngine::Texture2D::CreateTexture2D", "Width and height of texture must be more than 0.");
+        }
+
+        GLuint texId;
+        glGenTextures(1, &texId);
+        glBindTexture(GL_TEXTURE_2D, texId);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+        glPixelStorei(GL_PACK_ALIGNMENT, 4);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, w, h, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+        Texture2D* tex = new Texture2D(texId, w, h);
+        rm->AddResource(tex);
+        return tex;
+    }
+}
