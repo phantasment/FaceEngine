1f292a8d232232b1fd1bd0c15fb30e0908bdc30e
silentptr <imquiet07@gmail.com>
2022-05-15 16:14:23 +1000

    documentation

--- include/FaceEngine/Game.h
+++ include/FaceEngine/Game.h
@@ -3,7 +3,7 @@
  * @author Logan Woods
  * @brief A class containing the logic required to run a game.
  * @version 0.1
- * @date 2022-04-27
+ * @date 2022-05-15
  * 
  * @copyright Copyright (c) 2022
  * 
@@ -23,43 +23,99 @@
 
 namespace FaceEngine
 {
+    /**
+     * @brief The object responsible for running the game's code.
+     * 
+     * This object should be inherited and the game's logic should be implemented by overriding the Initialise(), Update(), and Draw() functions.
+     */
     class Game
     {
     private:
         static std::atomic_bool GameRunning;
-        void RunGame();
     protected:
-        // The graphics device for a running game;
+        /**
+         * @brief An interface for the graphics capabilities for the current computer.
+         * 
+         * This object's allocation is automatic.
+         */
         GraphicsDevice* GraphicsDevicePtr;
 
-        // Manages heap allocated resources such as textures, fonts, shaders, & sound.
+        /**
+         * @brief A manager for heap-allocated objects used for the game.
+         * 
+         * This object's allocation is automatic.
+         */
         ResourceManager* ResourceManagerPtr;
         
-        // The game window.
+        /**
+         * @brief An interface for the game window.
+         * 
+         * This object's allocation is automatic.
+         */
         Window* WindowPtr;
 
-        // The content loader.
+        /**
+         * @brief Loads game content from Face Engine content files.
+         * 
+         * This object's allocation is automatic.
+         */
         ContentLoader* ContentLoaderPtr;
         
-        // Contains ionformation important for updating a game such as input and delta time.
+        /**
+         * @brief Contains information used for updating a game.
+         * 
+         * This object's allocation is automatic.
+         */
         GameUpdate* GameUpdatePtr;
 
-        // Contains information important for drawing a game.
+        /**
+         * @brief Contains information used for drawing a game.
+         * 
+         * This object's allocation is automatic.
+         */
         GameDraw* GameDrawPtr;
 
-        // Determines the target speed of the Update() function.
-        // A value less than 1 indicates no limit.
+        /**
+         * @brief Determines the target speed of the Update() function. A value less than 1 indicates no limit.
+         */
         double PreferredUpdates = 0.0f;
 
-        // Determines the target speed of the Draw() function.
-        // A value less than 1 indicates no limit.
+        /**
+         * @brief Determines the target speed of the Draw() function. A value less than 1 indicates no limit.
+         */
         double PreferredDraws = 0.0f;
     public:
+        /**
+         * @brief Code to execute before the game starts running.
+         * 
+         * This function should never be called manually.
+         */
         virtual void Initialise();
+
+        /**
+         * @brief Code responsible for updating the game such as physics, input, etc.
+         * 
+         * This function should never be called manually.
+         */
         virtual void Update();
+
+        /**
+         * @brief Code responsible for drawing the game.
+         * 
+         * This function should never be called manually.
+         */
         virtual void Draw();
 
+        /**
+         * @brief Runs the game on the current thread.
+         * 
+         * An exception will be thrown if an error occurs while setting up the graphics environment.
+         */
         void Run();
+
+        /**
+         * @brief Once called, the game will close after finishing the current tick.
+         */
         void Exit();
     };
 }
